c3bc4611e59c35039d2de8269b3739a36e68585f
Victor Tsykanov <victor-tsykanov@users.noreply.github.com>
2025-03-21 12:09:56 +0300

    Add missing timestamps to orders table

--- internal/adapters/out/postgres/order/records.go
+++ internal/adapters/out/postgres/order/records.go
@@ -2,9 +2,11 @@ package order
 
 import (
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Order struct {
+	gorm.Model
 	ID        uuid.UUID  `gorm:"type:uuid;primaryKey"`
 	Location  Location   `gorm:"embedded;embeddedPrefix:location_"`
 	Status    string     `gorm:"type:varchar(100)"`
